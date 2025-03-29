b4f1d1f1beba95c1a7ecd29576062089ba7b69bb
Ronak Jain <ronakjain.public@gmail.com>
2025-03-29 19:08:02 +0530

    Set up repo

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { callAsyncFunction } from './async-function';
+import { wrapRequire } from './wrap-require';
+import * as tl from 'azure-pipelines-task-lib/task';
+import { getWebApiWithProxy } from "./web-api";
+
+process.on('unhandledRejection', handleError);
+
+async function run() {
+  const script: string | undefined = tl.getInput('script', true);
+  const maxRetriesInput: string | undefined = tl.getInput('maxRetries', false);
+
+  const maxRetries = isNaN(Number(maxRetriesInput)) ? 5 : Number(maxRetriesInput);
+  const webApi = getWebApiWithProxy({ allowRetries: maxRetries > 0, maxRetries });
+  
+  if (!script) {
+    throw new Error('Script input is required');
+  }
+
+  const result = await callAsyncFunction(
+    {
+      require: wrapRequire,
+      webApi,
+      tl
+    },
+    script
+  );
+
+  tl.setResult(tl.TaskResult.Succeeded, 'Script executed successfully');
+  if (result !== undefined) {
+    tl.setVariable('result', typeof result === "string" ? result : JSON.stringify(result), false, true);
+  }
+}
+
+function handleError(err: any): void {
+  tl.setResult(tl.TaskResult.Failed, `Unhandled error: ${err.message}`);
+  console.error(err)
+}
+
+run().catch(handleError);
